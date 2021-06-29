ebc1a6be7fd2dc129c32b9f986b3b26c387ef90f
Sushmita Poudel <66173400+sushmita56@users.noreply.github.com>
2021-06-29 12:34:38 +0545

    Update settings.py

--- projectSeventh/ProjectSeventh/settings.py
+++ projectSeventh/ProjectSeventh/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rc%tk$!5ve&(abknk11_fak0cmd@mn@%au&oq3g@09%x*wj*!i'
+SECRET_KEY = 'your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
