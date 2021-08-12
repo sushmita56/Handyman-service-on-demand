67caa9c2fd3e6af96984cd73c513eab089946aff
Sushmita Poudel <poudelsushmita878@gmail.com>
2021-08-12 14:13:59 +0545

    initial commit

--- projectSeventh/ProjectSeventh/settings.py
+++ projectSeventh/ProjectSeventh/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your secret key'
+SECRET_KEY = os.environ['django_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,7 +128,7 @@ STATICFILES_DIRS = [
 #To send email notiifcations
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "hsod2021@gmail.com"
-EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = os.environ["email_address"]
+EMAIL_HOST_USER = os.environ["email_address"]
+EMAIL_HOST_PASSWORD = os.environ["password"]
 EMAIL_USE_TLS = True
